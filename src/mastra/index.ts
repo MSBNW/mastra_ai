e1ce0a0946a9c9b6e3a4287e84512ea87d840d10
Bogdan <maslyb.tf@gmailc.om>
2025-03-12 12:14:15 +0200

    add Voice agent (first version)

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,13 +1,13 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { weatherWorkflow } from './workflows';
-import { weatherAgent } from './agents';
-import {whoisAgent} from "./agents/whoisAgent";
+import { weatherAgent, mainVoiceAgent } from './agents';
+import { whoisAgent } from "./agents/whoisAgent";
 import { VercelDeployer } from '@mastra/deployer-vercel';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
-  agents: { weatherAgent, whoisAgent },
+  agents: { weatherAgent, whoisAgent, mainVoiceAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
